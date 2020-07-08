8b00588cb5c6934ef460fd3106c6c2faa84f3e64
Nikita Kovrigin <nikita.kovrigin@peaks.com>
2020-07-08 09:26:44 +0200

    Fix nozzle's shape

--- space/nozzle.cpp
+++ space/nozzle.cpp
@@ -1,6 +1,6 @@
 #include "nozzle.hpp"
-
 #include "ship.hpp"
+
 
 namespace space {
 Nozzle::Nozzle(Ship const& ship, primitive::Point coordinate)
@@ -19,30 +19,35 @@ double Nozzle::getTiltAngel() const
 
 void Nozzle::update()
 {
-    // double offsetLength = ship_.get_speed().get_current_a();
     double offsetLength = ship_.get_speed().get_current_a_signed();
     colors.update_(ship_.get_speed().get_current_a_signed());
     border_.clear();
     std::copy(border_origin_.begin(), border_origin_.end(),
         std::back_inserter(border_));
     double Cx, Cy, Cx2, Cy2;
-    int multiply = -1;
-    
-    if (offsetLength < 0) {
-        multiply = 1;
-    }
+
+    bool flag{offsetLength < 0 ? false: true};
     offsetLength = std::abs(offsetLength);
     
     Cx = (border_[1].x - border_[0].x) * offsetLength;
-    Cy = (border_[1].y - border_[0].y) * multiply * offsetLength;
+    Cy = (border_[1].y - border_[0].y) * offsetLength;
 
     Cx2 = (border_[0].x - border_[2].x) * offsetLength;
-    Cy2 = (border_[0].y - border_[2].y) * multiply * offsetLength;
-    border_[1].x += Cx;
-    border_[1].y += Cy;
+    Cy2 = (border_[0].y - border_[2].y) * offsetLength;
 
-    border_[2].x -= Cx2;
-    border_[2].y -= Cy2;
+    if (flag) {
+        border_[1].x += Cx2;
+        border_[1].y += Cy2;
+
+        border_[2].x -= Cx;
+        border_[2].y -= Cy;
+    } else {
+        border_[1].x += Cx;
+        border_[1].y += Cy;
+
+        border_[2].x -= Cx2;
+        border_[2].y -= Cy2;
+    }
 }
 
 void Nozzle::rotate(bool clockwise)
